5171fda3dd2b98c01d9abf63de50f8e986b72636
MitraKumar <kaushikmitra556@gmail.com>
2025-07-03 16:23:54 +0000

    Initial Commit
    - Project Setup, with rollup for typescript build.

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts', // Your entry file
+  output: [
+    {
+      file: 'dist/main.js',
+      format: 'cjs',
+      sourcemap: true
+    },
+  ],
+  plugins: [
+    typescript(),
+  ],
+  external: [] // List external dependencies here if needed
+};
